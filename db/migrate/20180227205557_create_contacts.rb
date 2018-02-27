c5bf928dc8f5fd8829704297eb9580ea67d287f3
Ian Harwell <sirharwell@gmail.com>
2018-02-27 13:56:25 -0700

    initial commit

---
+++ db/migrate/20180227205557_create_contacts.rb
@@ -0,0 +1,13 @@
+class CreateContacts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :contacts do |t|
+      t.string :name
+      t.integer :age
+      t.string :hair_color
+      t.string :gender
+      t.boolean :alive
+
+      t.timestamps
+    end
+  end
+end
